3e5dd9216b01ebb0e6afd73373591144332d5eee
Van J. Wilson <van.wilson1992@alumni.unc.edu>
2015-11-22 16:29:31 -0500

    Add basic Sass and JS building

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,43 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
 
 var paths = {
   src: {
-    html: 'src/index.html'
+    root: 'src',
+    html: 'src/**/*.html',
+    css:  'src/assets/css/**/*.scss',
+    js:  'src/assets/js/**/*.js'
   },
   dev: {
-    dest: 'dev_build'
+    root: 'dev_build',
+    css:  'dev_build/assets/css',
+    js:  'dev_build/assets/js'
   },
   prod: {
-    dest: 'prod_build'
+    dest: 'prod_build',
+    css:  'prod_build/assets/css'
   }
 };
 
 gulp.task('copy', function(){
   gulp.src(paths.src.html)
-    .pipe(gulp.dest(paths.dev.dest));
+    .pipe(gulp.dest(paths.dev.root));
 });
+
+// build app styles
+gulp.task('css:app', function() {
+  return gulp.src(paths.src.root + '/assets/css/main.scss')
+    .pipe(sass({
+      outputStyle: 'nested', // libsass doesn't support expanded yet
+      precision: 10
+    }))
+    .pipe(gulp.dest(paths.dev.css));
+});
+
+  // build app js
+  gulp.task('js:app', function() {
+    return gulp.src(paths.src.js)
+      .pipe(uglify('app.js'))
+      .pipe(gulp.dest(paths.dev.js));
+  });
