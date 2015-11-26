b47493ae9c35c89ef60a727bc47ceab1f616623b
Van J. Wilson <van.wilson1992@alumni.unc.edu>
2015-11-26 11:01:59 -0500

    Refine browserify process, intermediate stage

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,10 @@ var sass = require('gulp-sass');
 var uglify = require('gulp-uglify');
 var browserify = require('browserify');
 var babelify = require('babelify');
+var watchify = require('watchify');
 var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var sourcemaps = require('gulp-sourcemaps')
 
 /* nicer browserify errors */
 var util = require('gulp-util')
@@ -13,7 +16,8 @@ var paths = {
     root: 'src',
     html: 'src/**/*.html',
     css:  'src/assets/css/**/*.scss',
-    js:  'src/assets/js/**/*.js'
+    js:  'src/assets/js/**/*.js',
+    entry: 'src/assets/js/app.js'
   },
   dev: {
     root: 'dev_build',
@@ -35,23 +39,17 @@ gulp.task('copy', function(){
 gulp.task('css:app', function() {
   return gulp.src(paths.src.root + '/assets/css/main.scss')
     .pipe(sass({
+      sourcemap : true,
       outputStyle: 'nested', // libsass doesn't support expanded yet
       precision: 10
     }))
     .pipe(gulp.dest(paths.dev.css));
 });
 
-// build app js
-gulp.task('js:app', function() {
-  return gulp.src(paths.src.js)
-    .pipe(uglify())
-    .pipe(gulp.dest(paths.dev.js));
-});
-
 function map_error(err) {
   if (err.fileName) {
     // regular error
-    gutil.log(util.colors.red(err.name)
+    util.log(util.colors.red(err.name)
       + ': '
       + err.fileName.replace(__dirname + '/src/js/', '')
       + ': '
@@ -71,29 +69,86 @@ function map_error(err) {
 
 }
 
-function bundle_js(bundler) {
-  return bundler.bundle()
-    .on('error', map_error)
-    .pipe(source('app.js'))
-    // .pipe(buffer())
-    // .pipe(gulp.dest('app/dist'))
-    // .pipe(rename('app.min.js'))
-    // .pipe(sourcemaps.init({ loadMaps: true }))
-    //   // capture sourcemaps from transforms
-    //   .pipe(uglify())
-    // .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest(paths.dev.js))
+function compile(watch) {
+  var bundler = watchify(
+    browserify(paths.src.entry, {
+      debug: true
+    }).transform(babelify)
+  );
+
+  function rebundle() {
+    bundler.bundle()
+      .on('error', function(err) {
+        map_error(err);
+        this.emit('end');
+      })
+      .pipe(source('app.js'))
+      .pipe(buffer())
+      .pipe(sourcemaps.init({
+        loadMaps: true
+      }))
+      .pipe(uglify())
+      .pipe(sourcemaps.write('.'))
+      .pipe(gulp.dest(paths.dev.js))
+  }
+
+  if (watch) {
+    bundler.on('update', function() {
+      console.log('-> rebundling...');
+      rebundle();
+    });
+  }
+
+  rebundle();
+
 }
 
-// Without watchify
-gulp.task('browserify', function () {
-  var bundler = browserify(paths.src.root + '/assets/js/app.js', { 
-    cache: {},
-    packageCache: {},
-    fullPaths: false,
-    debug: true
-  });
+function watch() {
+  return compile(true);
+}
 
-  return bundle_js(bundler);
-})
+gulp.task('browserify', function() {
+  return compile();
+});
+gulp.task('watchify', function() {
+  return watch();
+});
 
+gulp.task('build', ['copy', 'css:app', 'browserify']);
+gulp.task('watch', ['copy', 'css:app', 'watchify']);
+
+
+gulp.task('default', ['watch']);
+
+// // build app js
+// gulp.task('js:app', function() {
+//   return gulp.src(paths.src.js)
+//     .pipe(uglify())
+//     .pipe(gulp.dest(paths.dev.js));
+// });
+
+// function bundle_js(bundler) {
+//   return bundler.bundle()
+//     .on('error', map_error)
+//     .pipe(source('app.js'))
+//     // .pipe(buffer())
+//     // .pipe(gulp.dest('app/dist'))
+//     // .pipe(rename('app.min.js'))
+//     // .pipe(sourcemaps.init({ loadMaps: true }))
+//     //   // capture sourcemaps from transforms
+//     //   .pipe(uglify())
+//     // .pipe(sourcemaps.write('.'))
+//     .pipe(gulp.dest(paths.dev.js))
+// }
+
+// // Without watchify
+// gulp.task('browserify', function () {
+//   var bundler = browserify(paths.src.root + '/assets/js/app.js', { 
+//     cache: {},
+//     packageCache: {},
+//     fullPaths: false,
+//     debug: true
+//   });
+
+//   return bundle_js(bundler);
+// });
